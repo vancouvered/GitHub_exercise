3471036430a42d7b460d5bc4ae9732766f32c844
vancouvered <shorihan@gmail.com>
2014-03-30 15:58:32 +0900

    Modified: mysql_connect() function -> mysqli_connect()

--- db_test.php
+++ db_test.php
@@ -1,7 +1,7 @@
-<!--?php <br ?--> $link = mysql_connect('localhost', 'root', '');
+<!--?php <br ?--> $link = mysqli_connect('localhost', 'root', '');
 if (!$link) {
 die('接続失敗！
-'.mysql_error());
+'.mysqli_error());
 } else {
 echo '接続成功！';
 }
